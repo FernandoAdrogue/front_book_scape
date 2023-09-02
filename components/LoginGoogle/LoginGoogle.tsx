a80fce55df6ad39a59533e679f61edcc43cba559
zimlama <zimlama@gmail.com>
2023-09-02 16:11:53 -0500

    se agrega el URL de bookscapeback

--- components/LoginGoogle/LoginGoogle.tsx
+++ components/LoginGoogle/LoginGoogle.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
 import decodeJwt from './decodeJwt';
 
+const bookscapeback = process.env.NEXT_PUBLIC_BOOKSCAPEBACK; // Obtiene la URL base del archivo .env.local
+
 export default function LoginGoogle() {
     const [nombre, setNombre] = useState<string | null>(null);
 
@@ -13,11 +15,15 @@ export default function LoginGoogle() {
     async function handleSuccess(credentialResponse: CredentialResponse) {
         console.log("credentialResponse", credentialResponse);
         if (credentialResponse.credential) {
-            const response = await fetch("Colocar ruta aqui", {
+            const credenciales = {
+                token: credentialResponse.credential
+            }
+
+            const response = await fetch(`${bookscapeback}/users/login`, {
                 method: "POST",
-                body: JSON.stringify({
-                    token: credentialResponse.credential
-                })
+                body: JSON.stringify (
+                    credenciales
+                )
             });
             const { payload } = decodeJwt(credentialResponse.credential)
             console.log("payload credential", payload);
@@ -29,5 +35,5 @@ export default function LoginGoogle() {
         <div>
             {nombre === null && <GoogleLogin useOneTap onError={handleError} onSuccess={handleSuccess} />}
             {nombre && <p>El usuario se ha iniciado sesion: {nombre}</p>}
-        </div>
-    )}
+        </div>
+    )}
