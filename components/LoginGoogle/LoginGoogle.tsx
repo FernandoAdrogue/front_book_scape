0360fb585c2dbd0644ffe470063b84c0f077c95c
Andrés Ojeda <ojedaandres37@gmail.com>
2023-09-03 11:50:54 -0500

    primera ruta de logueo

--- components/LoginGoogle/LoginGoogle.tsx
+++ components/LoginGoogle/LoginGoogle.tsx
@@ -1,41 +1,48 @@
 import React, { useState } from 'react';
 import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
 import decodeJwt from './decodeJwt';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 
 const bookscapeback = process.env.NEXT_PUBLIC_BOOKSCAPEBACK; // Obtiene la URL base del archivo .env.local
 
 export default function LoginGoogle() {
+    
     const [nombre, setNombre] = useState<string | null>(null);
+    const router = useRouter();
 
     function handleError() {
         console.log("Falla del login Google");
-
     }
 
     async function handleSuccess(credentialResponse: CredentialResponse) {
-        console.log("credentialResponse", credentialResponse);
         if (credentialResponse.credential) {
+            
             const credenciales = {
                 token: credentialResponse.credential
             }
 
-            const response = await fetch(`${bookscapeback}/users/login`, {
-                method: "POST",
-                body: JSON.stringify (
-                    credenciales
-                )
-            });
-            console.log("esto es response:", response);
             const { payload } = decodeJwt(credentialResponse.credential)
-            console.log("payload credential", payload);
-            setNombre(payload.nombre);
-            console.log("esto es despues de response:", response);
+            
+            setNombre(payload.email);
+
+            console.log("esto es payload.email: ", payload.email);
+
+            const response = await axios.post(`${bookscapeback}/users/googleloggin`, payload)
+            
+            onsole.log("esto es response: ", response);
+
+            router.push("/");
         }
     }
 
     return ( 
         <div>
+            
             {nombre === null && <GoogleLogin useOneTap onError={handleError} onSuccess={handleSuccess} />}
+            
             {nombre && <p>El usuario se ha iniciado sesion: {nombre}</p>}
+            
         </div>
-    )}
+        )
+    }
