1f33d311b0a172fd73508bf6e139000f389e9e47
zimlama <zimlama@gmail.com>
2023-09-05 11:14:56 -0500

    se agrega href={/filtrar} en la linea 134

--- pages/book/[id].tsx
+++ pages/book/[id].tsx
@@ -131,7 +131,7 @@ const DetallesBook = () => {
                     />
                   </div>
                   <div>
-                    <Link href={`/`}>
+                    <Link href={"/filtrar"}>
                       <button className={styles.button}>Regresar</button>
                     </Link>
                   </div>
